b520441589e575c78778a308b740fdf132b9485e
NeoDev <neoforevershog@gmail.com>
2025-02-10 16:11:36 +0700

    Create theme loader settings

---
+++ tabs/theme_loader.py
@@ -0,0 +1,19 @@
+import os 
+import gradio as gr
+import assets.themes.loadThemes as loadThemes
+
+def theme_loader():
+  gr.Markdowm("Theme Loader for UI")
+  themes_select = gr.Dropdown(
+    label = "Theme",
+    info = "Select the theme you want to use. (Requires restarting the App)",
+    choices = loadThemes.get_list(),
+    value = loadThemes.read_json(),
+    visible = True
+  )
+  goofy_output = gr.Textbox(visible = False)
+  themes_select.change(
+    fn = loadThemes.select_theme,
+    inputs = themes_select,
+    outputs = [goofy_output]
+  )
