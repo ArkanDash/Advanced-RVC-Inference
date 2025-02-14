47cf99e00b6bd8f60fbb64e1ec38d1bc7a20bda2
NeoDev <neoforevershog@gmail.com>
2025-02-14 12:39:31 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,87 +1,61 @@
 import gradio as gr
-import sys
-import os
-import logging
-from typing import Any
-DEFAULT_SERVER_NAME = "127.0.0.1"
-DEFAULT_PORT = 6969
-MAX_PORT_ATTEMPTS = 10
-import assets.themes.loadThemes as loadThemes
-# Set up logging
-logging.getLogger("uvicorn").setLevel(logging.WARNING)
-logging.getLogger("httpx").setLevel(logging.WARNING)
+import sys, os
+from tabs.full_inference import full_inference_tab
+from tabs.download_model import download_model_tab
+from tabs.download_music import download_music_tab
+from tabs.settings import select_themes_tab, lang_tab, restart_tab
 
-# Add current directory to sys.path
 now_dir = os.getcwd()
 sys.path.append(now_dir)
+DEFAULT_PORT = 7755
+MAX_PORT_ATTEMPTS = 10
 
-# Zluda hijack
-import rvc.lib.zluda
+from assets.i18n.i18n import I18nAuto
 
-# Import Tabs
-from tabs.inference.inference import inference_tab
-from tabs.download.download import download_tab
-from tabs.theme_loader.theme_loader import theme_loader
-# Run prerequisites
-from rvc.lib.tools.prerequisites_download import prerequisites_download_pipeline
-prerequisites_download_pipeline(models=True, exe=True)
+i18n = I18nAuto()
 
+import assets.themes.loadThemes as loadThemes
 
+rvc_theme = loadThemes.load_json() or "NoCrypt/miku"
 
-
-
-# Define Gradio interface
 with gr.Blocks(
-    theme = loadThemes.load_json() or "NoCrypt/miku", title="Advanced-RVC-Inference", css="footer{display:none !important}"
-) as adrvc:
+    theme=rvc_theme, title="Advanced-RVC-Inference"
+) as rvc:
     gr.Markdown("# Advanced-RVC-Inference")
-    gr.Markdown("Advanced RVC Inference for quicker and effortless model downloads.")
-    gr.Markdown("[Support](https://discord.gg/hvmsukmBHE) — [GitHub](https://github.com/ArkanDash/Advanced-RVC-Inference.git)")
-    gr.Markdown("Thanks to [NeoDev](https://github.com/TheNeodev) for improve this project")
-    
-    
-    with gr.Tab("Inference"):
-        inference_tab()
-    with gr.Tab("Download"):
-        download_tab()
-
-    with gr.Tab("Settings"):
-        theme_loader()
-
-    gr.Markdown(
-    """
-    <div style="text-align: center; font-size: 0.9em; color: #a3a3a3;">
-        <strong>Disclaimer:</strong> By accessing <span style="color: #555;">Advanced-RVC-Inference</span>, you acknowledge your responsibility to follow all ethical and legal standards, respect intellectual property and privacy rights, and accept full accountability for your actions. <br><br>
-        Please note that Advanced-RVC-Inference disclaims all liability and reserves the right to update these terms at any time.
-    </div>
-    """
-)
+    with gr.Tab(i18n("Full Inference")):
+        full_inference_tab()
+    with gr.Tab(i18n("Download Music")):
+        download_music_tab()
+    with gr.Tab(i18n("Download Model")):
+        download_model_tab()
+    with gr.Tab(i18n("Settings")):
+        select_themes_tab()
+        lang_tab()
+        restart_tab()
+        
 
 
-def launch_gradio(server_name: str, server_port: int) -> None:
-    adrvc.launch(
+def launch(port):
+    rvc.launch(
         share="--share" in sys.argv,
         inbrowser="--open" in sys.argv,
-        server_name=server_name,
-        server_port=server_port,
+        server_port=port,
     )
 
 
-def get_value_from_args(key: str, default: Any = None) -> Any:
-    if key in sys.argv:
-        index = sys.argv.index(key) + 1
-        if index < len(sys.argv):
-            return sys.argv[index]
-    return default
+def get_port_from_args():
+    if "--port" in sys.argv:
+        port_index = sys.argv.index("--port") + 1
+        if port_index < len(sys.argv):
+            return int(sys.argv[port_index])
+    return DEFAULT_PORT
 
 
 if __name__ == "__main__":
-    port = int(get_value_from_args("--port", DEFAULT_PORT))
-    server = get_value_from_args("--server-name", DEFAULT_SERVER_NAME)
-
+    port = get_port_from_args()
     for _ in range(MAX_PORT_ATTEMPTS):
         try:
-            launch_gradio(server, port)
+            launch(port)
             break
         except OSError:
             print(
