b4a39c9d26fce3022b493009e5ad02d89353c4bc
programer01 <festyus09@gmail.com>
2025-11-24 21:03:05 +0700

    Update full_inference.py

--- advanced_rvc_inference/tabs/full_inference.py
+++ advanced_rvc_inference/tabs/full_inference.py
@@ -1,18 +1,20 @@
-from advanced_rvc_inference.core import full_inference_program
-import sys, os
-import gradio as gr
-import regex as re
-from advanced_rvc_inference.lib.i18n import I18nAuto
 import torch
 import warnings
+import sys, os
 warnings.filterwarnings("ignore", category=UserWarning)
 import shutil
+import gradio as gr
+import regex as re
 import unicodedata
 
 i18n = I18nAuto()
 
 now_dir = os.getcwd()
 sys.path.append(now_dir)
+
+from advanced_rvc_inference.core import full_inference_program
+from advanced_rvc_inference.lib.i18n import I18nAuto
+
 
 model_root = os.path.join(now_dir, "logs")
 audio_root = os.path.join(now_dir, "audio_files", "original_files")
