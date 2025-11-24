a761ec6344dbddd9bcfbbea26ea14e8830764284
programer01 <festyus09@gmail.com>
2025-11-24 21:13:29 +0700

    Update main.py

--- advanced_rvc_inference/main.py
+++ advanced_rvc_inference/main.py
@@ -19,9 +19,9 @@ from advanced_rvc_inference.tabs.download_music import download_music_tab
 from advanced_rvc_inference.tabs.settings import select_themes_tab
 from advanced_rvc_inference.tabs.training import training_tab
 from advanced_rvc_inference.tabs.model_manager import model_manager_tab
-from tabs.enhancement import enhancement_tab
+from advanced_rvc_inference tabs.enhancement import enhancement_tab
 from advanced_rvc_inference.tabs.real_time import real_time_inference_tab
-from tadvanced_rvc_inference.abs.config_options import extra_options_tab
+from tadvanced_rvc_inference.tabs.config_options import extra_options_tab
 
 # Attempt to import additional advanced features from Applio and Vietnamese-RVC
 try:
