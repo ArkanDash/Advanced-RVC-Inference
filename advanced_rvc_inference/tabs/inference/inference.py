4a1c25b35e2255aa4f0eddcc4d0f5a4116745f61
programer01 <festyus09@gmail.com>
2025-12-07 20:34:37 +0700

    Update inference.py

--- advanced_rvc_inference/tabs/inference/inference.py
+++ advanced_rvc_inference/tabs/inference/inference.py
@@ -15,7 +15,7 @@ def inference_tab():
 	with gr.TabItem(translations["convert_audio"], visible=configs.get("convert_tab", True))
             gr.Markdown(f"## {translations['convert_audio']}")
             convert_tab()
-    with gr.TabItem(translations["inference"], visible=configs.get("inference_tab", True)):
+    with gr.TabItem(translations["inference"], visible=configs.get("inference_tab", True))
         with gr.TabItem(translations["separator_tab"], visible=configs.get("separator_tab", True)
             gr.Markdown(f"## {translations['separator_tab']}")
             separate_tab()    
