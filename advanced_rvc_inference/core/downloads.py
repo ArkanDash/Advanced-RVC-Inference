bee4e8ea18d94b4e1edfa3b79220dbb3fba490dc
programer01 <festyus09@gmail.com>
2025-12-07 23:56:54 +0700

    Update downloads.py

--- advanced_rvc_inference/core/downloads.py
+++ advanced_rvc_inference/core/downloads.py
@@ -15,7 +15,7 @@ sys.path.append(os.getcwd())
 from advanced_rvc_inference.tools import huggingface, gdown, meganz, mediafire, pixeldrain
 from advanced_rvc_inference.core.process import move_files_from_directory, fetch_pretrained_data, extract_name_model
 from advanced_rvc_inference.core.ui import gr_info, gr_warning
-from advanced_rvc_inference.variables import config, translations, configs
+from advanced_rvc_inference.variables import logger, translations, model_options, configs
 
 def download_url(url):
     if not url: 
