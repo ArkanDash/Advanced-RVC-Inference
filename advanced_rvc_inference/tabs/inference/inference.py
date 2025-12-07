8bb559969a839b786f658dcadcb6cb928a1db425
programer01 <festyus09@gmail.com>
2025-12-07 20:33:46 +0700

    Update inference.py

--- advanced_rvc_inference/tabs/inference/inference.py
+++ advanced_rvc_inference/tabs/inference/inference.py
@@ -12,20 +12,18 @@ from advanced_rvc_inference.tabs.inference.child.convert_tts import convert_tts_
 from advanced_rvc_inference.tabs.inference.child.convert_with_whisper import convert_with_whisper_tab
 
 def inference_tab():
-	with gr.TabItem(translations["convert_audio"], visible=configs.get("convert_tab", True)):
+	with gr.TabItem(translations["convert_audio"], visible=configs.get("convert_tab", True))
             gr.Markdown(f"## {translations['convert_audio']}")
             convert_tab()
-    with gr.TabItem(translations["inference"], visible=configs.get("inference_tab", True):
-        with gr.TabItem(translations["separator_tab"], visible=configs.get("separator_tab", True)):
+    with gr.TabItem(translations["inference"], visible=configs.get("inference_tab", True)):
+        with gr.TabItem(translations["separator_tab"], visible=configs.get("separator_tab", True)
             gr.Markdown(f"## {translations['separator_tab']}")
-            separate_tab()
+            separate_tab()    
 
-        
-
-        with gr.TabItem(translations["convert_with_whisper"], visible=configs.get("convert_with_whisper", True)):
+        with gr.TabItem(translations["convert_with_whisper"], visible=configs.get("convert_with_whisper", True))
             gr.Markdown(f"## {translations['convert_with_whisper']}")
             convert_with_whisper_tab()
 
-        with gr.TabItem(translations["convert_text"], visible=configs.get("tts_tab", True)):
+        with gr.TabItem(translations["convert_text"], visible=configs.get("tts_tab", True))
             gr.Markdown(translations["convert_text_markdown"])
             convert_tts_tab()
