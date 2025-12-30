6f5666a0b3f8dac798aa56fe5f91407921225eec
BF667 <BF667@users.noreply.github.com>
2025-12-30 13:18:42 +0800

    Clean up duplicate and unwanted files
    - Removed advanced-rvc-inference/ nested directory structure
    - Removed browser/ directory with browser extension files
    - All vocoder files now properly integrated in main project structure

--- advanced-rvc-inference/advanced_rvc_inference/library/algorithm/conformer/__init__.py
+++
@@ -1,1 +0,0 @@
-from .conformer import ConformerConvModule, ConformerBlock, Conformer
--- advanced-rvc-inference/advanced_rvc_inference/library/algorithm/generators/__init__.py
+++
@@ -1,6 +0,0 @@
-from .ringformer import RingFormerGenerator
-from .hifigan_nsf import HiFiGANNSFGenerator
-from .pcph_gan import PCPH_GAN_Generator
-from .hifigan_mrf import HiFiGANMRFGenerator
-from .hifigan import HiFiGANGenerator
-from .refinegan import RefineGANGenerator
