31ae97d4c51c74cc5ee3188a2b353fee6b2ac788
programer01 <festyus09@gmail.com>
2026-01-07 15:12:26 +0700

    fix

--- advanced_rvc_inference/rvc/train/extracting/extract.py
+++ advanced_rvc_inference/rvc/train/extracting/extract.py
@@ -11,8 +11,8 @@ from distutils.util import strtobool
 sys.path.append(os.getcwd())
 
 from advanced_rvc_inference.library.utils import check_assets
-from advanced_rvc_inference.extracting.rms import run_rms_extraction
-from advanced_rvc_inference.extracting.feature import run_pitch_extraction
+from advanced_rvc_inference.rvc.train.extracting.rms import run_rms_extraction
+from advanced_rvc_inference.rvc.train.extracting.feature import run_pitch_extraction
 from advanced_rvc_inference.utils.variables import config, logger, translations, configs
 from advanced_rvc_inference.extracting.embedding import run_embedding_extraction
 from advanced_rvc_inference.extracting.preparing_files import generate_config, generate_filelist
