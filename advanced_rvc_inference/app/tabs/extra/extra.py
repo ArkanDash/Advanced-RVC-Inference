5e03fdc5b2f9ee2f063b544e41ce114deae4cf92
BF667 <215091179+BF667@users.noreply.github.com>
2026-01-11 14:38:39 +0700

    Update extra.py

--- advanced_rvc_inference/app/tabs/extra/extra.py
+++ advanced_rvc_inference/app/tabs/extra/extra.py
@@ -11,7 +11,7 @@ from advanced_rvc_inference.app.tabs.extra.child.read_model import read_model_ta
 from advanced_rvc_inference.app.tabs.extra.child.f0_extract import f0_extract_tab
 from advanced_rvc_inference.app.tabs.extra.child.create_srt import create_srt_tab
 from advanced_rvc_inference.app.tabs.extra.child.convert_model import convert_model_tab
-from main.app.tabs.extra.child.settings import settings_tab
+from advanced_rvc_inference.app.tabs.extra.child.settings import settings_tab
 
 def extra_tab(app):
     with gr.TabItem(translations["extra"], visible=configs.get("extra_tab", True)):
