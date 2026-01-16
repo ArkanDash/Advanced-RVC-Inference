0e3b697b8cbb253d9c5f4f237aebab93ef4f0f74
BF667 <215091179+BF667@users.noreply.github.com>
2026-01-16 11:17:34 +0700

    Update __init__.py

--- advanced_rvc_inference/app/__init__.py
+++ advanced_rvc_inference/app/__init__.py
@@ -2,5 +2,8 @@
 # Main application entry points (GUI, CLI, etc.)
 
 from .gui import launch as launch_gui
+from .ez import launch as launch_ez
 
-__all__ = ['launch_gui']
+
+
+__all__ = ['launch_gui', launch_ez']
