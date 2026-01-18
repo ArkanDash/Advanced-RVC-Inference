11182a61e845e8ccce70d3ecff1e950f06f50829
programer01 <festyus09@gmail.com>
2026-01-18 21:10:56 +0700

    Update RMVPE.py

--- advanced_rvc_inference/library/predictors/RMVPE/RMVPE.py
+++ advanced_rvc_inference/library/predictors/RMVPE/RMVPE.py
@@ -12,7 +12,7 @@ from advanced_rvc_inference.library.predictors.RMVPE.mel import MelSpectrogram
 N_MELS, N_CLASS = 128, 360
 
 class RMVPE:
-    def __init__(self, model_path, is_half, device=None, providers=None, onnx=False):
+    def __init__(self, model_path, is_half, device=None, providers=None, onnx=False, hpa=False):
         self.onnx = onnx
 
         if self.onnx:
@@ -22,39 +22,49 @@ class RMVPE:
             sess_options.log_severity_level = 3
             self.model = ort.InferenceSession(model_path, sess_options=sess_options, providers=providers)
         else:
-            from advanced_rvc_inference.library.predictors.RMVPE.e2e import E2E
+            from main.library.predictors.RMVPE.e2e import E2E
+            model = E2E(4, 1, (2, 2), 5, 4, 1, 16, hpa=hpa)
 
-            model = E2E(4, 1, (2, 2))
-            ckpt = torch.load(model_path, map_location="cpu", weights_only=True)
-            model.load_state_dict(ckpt)
+            model.load_state_dict(torch.load(model_path, map_location="cpu", weights_only=True))
             model.eval()
             if is_half: model = model.half()
             self.model = model.to(device)
 
-        self.is_half = is_half
         self.device = device
+        self.is_half = is_half
         self.mel_extractor = MelSpectrogram(N_MELS, 16000, 1024, 160, None, 30, 8000).to(device)
         cents_mapping = 20 * np.arange(N_CLASS) + 1997.3794084376191
         self.cents_mapping = np.pad(cents_mapping, (4, 4))
 
-    def mel2hidden(self, mel):
+    def mel2hidden(self, mel, chunk_size = 32000):
         with torch.no_grad():
             n_frames = mel.shape[-1]
-            n_pad = 32 * ((n_frames - 1) // 32 + 1) - n_frames
-            if n_pad > 0: mel = F.pad(mel, (0, n_pad), mode="constant")
-            
-            if self.onnx:
-                hidden = self.model.run(
-                    [self.model.get_outputs()[0].name], 
-                    {
-                        self.model.get_inputs()[0].name: mel.cpu().numpy().astype(np.float32)
-                    }
-                )[0] 
-            else: 
-                hidden = self.model(
-                    mel.half() if self.is_half else mel.float()
-                )
+            mel = F.pad(mel, (0, 32 * ((n_frames - 1) // 32 + 1) - n_frames), mode="reflect")
 
+            output_chunks = []
+            pad_frames = mel.shape[-1]
+
+            for start in range(0, pad_frames, chunk_size):
+                mel_chunk = mel[..., start:min(start + chunk_size, pad_frames)]
+                assert mel_chunk.shape[-1] % 32 == 0
+
+                if self.onnx:
+                    mel_chunk = mel_chunk.cpu().numpy().astype(np.float32)
+
+                    out_chunk = torch.as_tensor(
+                        self.model.run(
+                            [self.model.get_outputs()[0].name], 
+                            {self.model.get_inputs()[0].name: mel_chunk}
+                        )[0], 
+                        device=self.device
+                    )
+                else: 
+                    if self.is_half: mel_chunk = mel_chunk.half()
+                    out_chunk = self.model(mel_chunk)
+
+                output_chunks.append(out_chunk)
+
+            hidden = torch.cat(output_chunks, dim=1)
             return hidden[:, :n_frames]
 
     def decode(self, hidden, thred=0.03):
@@ -64,9 +74,16 @@ class RMVPE:
         return f0
 
     def infer_from_audio(self, audio, thred=0.03):
-        hidden = self.mel2hidden(self.mel_extractor(torch.from_numpy(audio).float().to(self.device).unsqueeze(0), center=True))
+        hidden = self.mel2hidden(
+            self.mel_extractor(
+                torch.from_numpy(audio).float().to(self.device).unsqueeze(0), center=True
+            )
+        )
 
-        return self.decode(hidden.squeeze(0).cpu().numpy().astype(np.float32) if not self.onnx else hidden[0], thred=thred)
+        return self.decode(
+            hidden.squeeze(0).cpu().numpy().astype(np.float32), 
+            thred=thred
+        )
     
     def infer_from_audio_with_pitch(self, audio, thred=0.03, f0_min=50, f0_max=1100):
         f0 = self.infer_from_audio(audio, thred)
