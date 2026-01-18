97300d25ea0cd1d4e727dc7bae95583de0183595
BF667 <215091179+BF667@users.noreply.github.com>
2026-01-18 21:12:18 +0700

    Update mel.py

--- advanced_rvc_inference/library/predictors/RMVPE/mel.py
+++ advanced_rvc_inference/library/predictors/RMVPE/mel.py
@@ -11,7 +11,17 @@ from librosa.filters import mel
 sys.path.append(os.getcwd())
 
 class MelSpectrogram(nn.Module):
-    def __init__(self, n_mel_channels, sample_rate, win_length, hop_length, n_fft=None, mel_fmin=0, mel_fmax=None, clamp=1e-5):
+    def __init__(
+        self, 
+        n_mel_channels, 
+        sample_rate, 
+        win_length, 
+        hop_length, 
+        n_fft=None, 
+        mel_fmin=0, 
+        mel_fmax=None, 
+        clamp=1e-5
+    ):
         super().__init__()
         n_fft = win_length if n_fft is None else n_fft
         self.hann_window = {}
@@ -28,6 +38,7 @@ class MelSpectrogram(nn.Module):
     def forward(self, audio, keyshift=0, speed=1, center=True):
         factor = 2 ** (keyshift / 12)
         win_length_new = int(np.round(self.win_length * factor))
+
         keyshift_key = str(keyshift) + "_" + str(audio.device)
         if keyshift_key not in self.hann_window: self.hann_window[keyshift_key] = torch.hann_window(win_length_new).to(audio.device)
 
@@ -37,15 +48,31 @@ class MelSpectrogram(nn.Module):
         if str(audio.device).startswith(("ocl", "privateuseone")):
             if not hasattr(self, "stft"): 
                 from main.library.backends.utils import STFT
-                self.stft = STFT(filter_length=n_fft, hop_length=hop_length, win_length=win_length_new).to(audio.device)
+
+                self.stft = STFT(
+                    filter_length=n_fft, 
+                    hop_length=hop_length, 
+                    win_length=win_length_new
+                ).to(audio.device)
+
             magnitude = self.stft.transform(audio, 1e-9)
         else:
-            fft = torch.stft(audio, n_fft=n_fft, hop_length=hop_length, win_length=win_length_new, window=self.hann_window[keyshift_key], center=center, return_complex=True)
+            fft = torch.stft(
+                audio, 
+                n_fft=n_fft, 
+                hop_length=hop_length, 
+                win_length=win_length_new, 
+                window=self.hann_window[keyshift_key], 
+                center=center, 
+                return_complex=True
+            )
+
             magnitude = (fft.real.pow(2) + fft.imag.pow(2)).sqrt()
 
         if keyshift != 0:
             size = self.n_fft // 2 + 1
             resize = magnitude.size(1)
+
             if resize < size: magnitude = F.pad(magnitude, (0, 0, 0, size - resize))
             magnitude = magnitude[:, :size, :] * self.win_length / win_length_new
 
