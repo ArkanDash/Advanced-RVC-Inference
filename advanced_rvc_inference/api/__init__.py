38e75f8f429f839811438e54dad6668969814472
Rico Ardiansyah <festyus09@gmail.com>
2026-02-25 01:10:34 +0700

    Update __init__.py

--- advanced_rvc_inference/api/__init__.py
+++ advanced_rvc_inference/api/__init__.py
@@ -1,16 +1,10 @@
 # API Module
 # REST API endpoints and CLI interface
 
-from .api import RVCConfig, RVCModel, RVCInference, RVCTrainer, RVCRealtime
 from .cli_complete import main as cli_main
 from .cli import main as cli_main_basic
 
 __all__ = [
-    'RVCConfig', 
-    'RVCModel', 
-    'RVCInference', 
-    'RVCTrainer', 
-    'RVCRealtime',
     'cli_main',
     'cli_main_basic'
 ]
